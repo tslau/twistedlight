da026b66c0afe01042bf4afeed14c9606e4bfba4
tslau <timtimtlau@yahoo.com>
2020-06-20 20:36:15 -0400

    Add files via upload

---
+++ build/precache-manifest.6d5a90a7627da163b3772f2a818cded0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb0a4215f61900b8e78e9d7827488f0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b808e9939709bd13b012",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "05d727624554f5a2c2e4",
+    "url": "/static/js/2.4291ea57.chunk.js"
+  },
+  {
+    "revision": "0445d5183d0141ac77c6557b72598011",
+    "url": "/static/js/2.4291ea57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b808e9939709bd13b012",
+    "url": "/static/js/main.14192394.chunk.js"
+  },
+  {
+    "revision": "5b48325df3c4f5d95641",
+    "url": "/static/js/runtime-main.81592254.js"
+  }
+]);
